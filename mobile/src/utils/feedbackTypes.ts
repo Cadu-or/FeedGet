b11068d3024b06d1d04e70fd1d8ae0be5c3f9561
cadu-or <180099094@aluno.unb.br>
2022-06-28 01:26:54 -0300

    Mobile iniciado

---
+++ mobile/src/utils/feedbackTypes.ts
@@ -0,0 +1,15 @@
+export const feedbackTypes = {
+  'BUG': {
+    title: 'Problema',
+    image: require('../assets/problema.svg')
+  },
+  'IDEA': {
+    title: 'Ideia',
+    image: require('../assets/idea.svg'),
+  },
+  'OTHER': {
+    title: 'Outro',
+    image: require('../assets/outro.svg'),
+  },
+};
+
